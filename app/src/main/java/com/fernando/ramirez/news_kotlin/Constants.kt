ffce0c750d3a8e2dc8d52ab95e0cfdc0b1be5398
Fernando Ramirez <fernando@revunit.com>
2017-07-19 23:11:18 -0500

    Fetch real news data

---
+++ app/src/main/java/com/fernando/ramirez/news_kotlin/Constants.kt
@@ -0,0 +1,9 @@
+package com.fernando.ramirez.news_kotlin
+
+/**
+ * Created by fernando on 7/19/17.
+ */
+
+object Constants {
+    val newsApiKey = "e227dcdc1fec434799afeacea50c6801" // API Key created with a temporary email from https://newsapi.org
+}
